933241fafeb97a0369d6944891db88105d05d936
Anuj Bajpai <anuj2901@gmail.com>
2023-12-19 11:37:46 +0530

    Loan_Application_Accessor.py

--- Loan_Application_Accessor.py
+++ Loan_Application_Accessor.py
@@ -1,27 +1,43 @@
 import streamlit as st
 import pandas as pd
-df_tr = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Loan_pred/Training Dataset.csv')
-#df_tr.drop(columns = ['Loan_Status','Loan_ID'_],inplace = True)
-df_tr.drop(columns = ['Loan_ID','Loan_Status'],inplace =True)
-
-Inp_Cols = df_tr.columns
+import pickle
+import json
+import numpy as np
+def Loan_app_pred(lst_1):
+    model_1 = pickle.load(open('finalized_model.sav', 'rb'))
+    if model_1.predict(lst_1) == 0:
+        return 'Your Loan is approved'
+    else :
+        return 'Your Loan is not Approved' 
+#df_tr = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Loan_pred/Training Dataset.csv')
+df_proc = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Df_train_mvr.csv') 
+#dictionary = json.loads('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/sample.json')
+with open('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/sample.json') as user_file:
+     dictionary = json.load(user_file)
+#print(dictionary)
+df_proc.drop(columns = ['Unnamed: 0','Loan_Status'],inplace =True)
+Inp_Cols = df_proc.columns
 st.title('Loan Approver app')
 st.header('Few Entries and know wheteher your loan will be approved or not ')
-#print(df_tr['Loan_Status'].unique()[0])
+#print(df_proc['Loan_Status'].unique()[0])
 inp_lst = []
 for cols in Inp_Cols:
      tmp_nm = f'{cols}'
-     inp_lst.append(tmp_nm)
-     if df_tr[cols].dtypes == 'object':
+     if df_proc[cols].dtypes == 'object':
        tmp_lst = []
-       tmp_1 = df_tr[cols].unique()
+       tmp_1 = df_proc[cols].unique()
        for i in tmp_1:
            tmp_lst.append(i)
-       
-       option = st.selectbox(tmp_nm,tmp_lst[0:-1])
+       option = st.selectbox(tmp_nm,tmp_lst)
+       for opts in dictionary[cols].keys():
+           if opts ==  option:
+               inp_lst.append(dictionary[cols][opts])
+               print(dictionary[cols],inp_lst)
      else :
-        tmp_nm = st.slider(f'{cols}',min_value = 0.0, max_value = float(df_tr[cols].max()))
-
+        option = st.slider(f'{cols}',min_value = 0.0, max_value = float(df_proc[cols].max()))
+        inp_lst.append(option)
+inp_lst = np.array(inp_lst).reshape(1,-1)
+print(Loan_app_pred(inp_lst))
           
 
 
