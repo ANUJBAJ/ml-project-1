fc0edd0b01033fbb8d12b795a967bccf7e89bd8c
Anuj Bajpai <anuj2901@gmail.com>
2023-12-21 17:24:54 +0530

    commiting a change

--- Loan_Application_Accessor.py
+++ Loan_Application_Accessor.py
@@ -9,21 +9,23 @@ def Loan_app_pred(lst_1):
         return 'Your Loan is approved'
     else :
         return 'Your Loan is not Approved' 
-#df_tr = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Loan_pred/Training Dataset.csv')
-#df_proc = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Df_train_mvr.csv') 
-df_proc = pd.read_csv('Df_train_mvr.csv') 
+df_tr = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Loan_pred/Training Dataset.csv')
+df_proc = pd.read_csv('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/Df_train_mvr.csv') 
+#df_proc = pd.read_csv('Df_train_mvr.csv') 
 #dictionary = json.loads('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/sample.json')
-#with open('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/sample.json') as user_file:
-with open('sample.json') as user_file:     
+with open('/home/anuj/Documents/ANUJ_Project/RADHA_My_frst_project_preplaced/sample.json') as user_file:
+#with open('sample.json') as user_file:     
      dictionary = json.load(user_file)
 #print(dictionary)
 df_proc.drop(columns = ['Unnamed: 0','Loan_Status'],inplace =True)
 Inp_Cols = df_proc.columns
+
 st.title('Loan Approver app')
 st.header('Few Entries and know wheteher your loan will be approved or not ')
 #print(df_proc['Loan_Status'].unique()[0])
 inp_lst = []
 for cols in Inp_Cols:
+     
      tmp_nm = f'{cols}'
      if df_proc[cols].dtypes == 'object':
        tmp_lst = []
@@ -33,13 +35,17 @@ for cols in Inp_Cols:
        option = st.selectbox(tmp_nm,tmp_lst)
        for opts in dictionary[cols].keys():
            if opts ==  option:
-               inp_lst.append(dictionary[cols][opts])
+               inp_lst.append(np.int64(dictionary[cols][opts]))
                
      else :
         option = st.slider(f'{cols}',min_value = 0.0, max_value = float(df_proc[cols].max()))
         inp_lst.append(option)
-inp_lst = np.array(inp_lst).reshape(1,-1)
-st.write(Loan_app_pred(inp_lst))
+print((np.array(inp_lst)).dtype)
+df_pred = pd.DataFrame(columns = Inp_Cols)
+df_pred.loc[0,:] = inp_lst
+print(df_pred)
+# inp_lst = np.array(inp_lst).reshape(1,-1)
+st.write(Loan_app_pred(np.array(df_pred.loc[0,:]).reshape(1,-1))) 
           
 
 
